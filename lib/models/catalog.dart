f39e5630f676f08d65ddfcf80d6fbdfdddd829ea
yadavvikas007 <56362252+yadavvikas007@users.noreply.github.com>
2021-05-18 22:18:02 +0530

    Day 8 commit , catalog.dart added

---
+++ lib/models/catalog.dart
@@ -0,0 +1,10 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
